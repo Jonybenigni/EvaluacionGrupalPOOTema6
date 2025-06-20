d1622951949d288d8fb044f6e24860c703b6f147
JonyBenigni <JonyBenigni@DESKTOP-5PM3964>
2025-06-20 20:54:46 -0300

    Creamos la biblioteca de Clase de Entidades

--- EvaluaciónGrupalPOOTema 6/Empleado.cs
+++ EvaluaciónGrupalPOOTema 6/Empleado.cs
@@ -2,6 +2,37 @@
 {
     public class Empleado
     {
+        public int Legajo { get; set; }
+
+        public string Nombre { get; set; } = null!;
+
+        public DateTime FechaIngreso { get; set; }
+
+        public double SueldoBase { get; set; }
+
+        public readonly double Antiguedad;
+
+        public Empleado(int legajo, string nombre,double antiguedad, DateTime fechaIngreso, double sueldoBase)
+        {
+            Legajo = legajo;
+            Nombre = nombre;
+            FechaIngreso = fechaIngreso;
+            SueldoBase = sueldoBase;
+            Antiguedad = antiguedad;
+        }
+
+        public double PorcentajePorAntiguedad()
+        {
+            double antiguedad = (DateTime.Now - FechaIngreso).TotalDays / 365; // Calcula la antigüedad en años
+            return antiguedad * 0.03; // Retorna el porcentaje de antigüedad
+        }
+
+        public double CalcularSueldo()
+        {
+            return SueldoBase;
+        }
+
+
 
 
 
---
+++ EvaluaciónGrupalPOOTema 6/Supervisor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static EvaluaciónGrupalPOOTema_6.Operario;
+
+namespace EvaluaciónGrupalPOOTema_6
+{
+    public class Supervisor : Empleado
+    {
+        public AreaEnum Area { get; set; }
+
+        public static int BonoFijo { get; set; } = 20000; 
+        public enum AreaEnum
+        {
+           Personal,
+           Fabrica,
+           Ventas
+
+        }
+
+        public Supervisor(int legajo, string nombre,AreaEnum area, double antiguedad, DateTime fechaIngreso, double sueldoBase) : base(legajo, nombre, antiguedad, fechaIngreso, sueldoBase)
+        {
+
+            Area = area;
+
+        }
+
+        public new double CalcularSueldo()
+        {
+            return SueldoBase + BonoFijo;
+        }
+    }
+}
