05b3d0e7a32adfd2380821f43d88a3001dbf1015
BiancaLopresto <bianlopre19@gmail.com>
2025-06-21 17:33:53 -0300

    Se Crearon Las Utilidades

---
+++ EvaluacionGrupal6.Utilidades/ExtensionesConsola.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvaluacionGrupal6.Utilidades
+{
+    public class ExtensionesConsola
+    {
+        public static void EscribirConColor(string texto, ConsoleColor color)
+        {
+            var anterior = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.WriteLine(texto);
+            Console.ForegroundColor = anterior;
+        }
+
+        public static void Titulo(string texto)
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("=== " + texto.ToUpper() + " ===");
+            Console.ResetColor();
+        }
+
+        public static void Separador()
+        {
+            Console.WriteLine(new string('-', 50));
+        }
+
+        public static void MensajeError(string mensaje)
+        {
+            EscribirConColor("ERROR: " + mensaje, ConsoleColor.Red);
+        }
+
+        public static void MensajeExito(string mensaje)
+        {
+            EscribirConColor("✔ " + mensaje, ConsoleColor.Green);
+        }
+    }
+}
