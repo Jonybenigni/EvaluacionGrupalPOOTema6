d9c85e03a28ebf025e6b5c71d31a27ca48543c9a
BiancaLopresto <bianlopre19@gmail.com>
2025-06-20 21:10:03 -0300

    Validaciones

--- EvaluaciónGrupalPOOTema 6/Empleado.cs
+++ EvaluaciónGrupalPOOTema 6/Empleado.cs
@@ -1,8 +1,11 @@
-﻿namespace EvaluaciónGrupalPOOTema_6
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace EvaluaciónGrupalPOOTema_6
 {
-    public class Empleado
+    public class Empleado : IValidatableObject
     {
-        public int Legajo { get; set; }
+        public string Legajo { get; set; }
 
         public string Nombre { get; set; } = null!;
 
@@ -12,7 +15,7 @@
 
         public readonly double Antiguedad;
 
-        public Empleado(int legajo, string nombre,double antiguedad, DateTime fechaIngreso, double sueldoBase)
+        public Empleado(string legajo, string nombre, double antiguedad, DateTime fechaIngreso, double sueldoBase)
         {
             Legajo = legajo;
             Nombre = nombre;
@@ -32,12 +35,27 @@
             return SueldoBase;
         }
 
+        public virtual IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            
+            if (string.IsNullOrWhiteSpace(Legajo) || !Regex.IsMatch(Legajo, @"^[A-Za-z]{2}[0-9]{3}$"))
+            {
+                yield return new ValidationResult("El legajo debe tener exactamente 2 letras seguidas de 3 números (ej. AB123).", new[] { nameof(Legajo) });
+            }
 
+            if (string.IsNullOrWhiteSpace(Nombre))
+            {
+                yield return new ValidationResult("El nombre no puede estar vacío ni contener solo espacios.", new[] { nameof(Nombre) });
+            }
 
+            if (Antiguedad < 0)
+            {
+                yield return new ValidationResult("La antigüedad no puede ser negativa.", new[] { nameof(Antiguedad) });
+            }
 
-
-
-
-
-    }
-}
+            if (FechaIngreso > DateTime.Now)
+            {
+                yield return new ValidationResult("La fecha de ingreso no puede ser superior a la fecha actual.", new[] { nameof(FechaIngreso) });
+            }
+        }
+    }  }
